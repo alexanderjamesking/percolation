c7e3e3fdbc87bb425e3b288a5db1c400f7c83d9c
Alex King <alex@alexanderjamesking.com>
2019-08-13 22:09:04 +0200

    svg rendering and random filling of squares

--- src/percolation/core.cljs
+++ src/percolation/core.cljs
@@ -1,36 +1,54 @@
 (ns percolation.core
-  (:require [reagent.core :as reagent]))
+  (:require [reagent.core :as reagent]
+            [clojure.core.async :as async :refer [chan <! >! timeout]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]]))
 
-(def id (reagent/atom [1 2 3 4 5 6 7 8 9]))
+(def grid-size 10)
+
+(def initial-state (zipmap (range (* grid-size grid-size)) (repeat :closed)))
+
+(def id (reagent/atom initial-state))
+
+(defn reset-state! []
+  (reset! id initial-state))
+
+(defn start! []
+  (reset-state!)
+  (go-loop [ids (shuffle (keys @id))]
+    (<! (timeout 100))
+    (if (empty? ids) ;; TODO: stop on percolate!
+      (do :done
+#_          (recur (shuffle (keys initial-state))))
+      (do
+        (swap! id assoc (first ids) :open)
+        (recur (rest ids))))))
 
 (defn view []
-  [:div
-   [:h1 "Hello world!"]
-
-   [:p (str @id)]
-
-
-   [:svg {:width 400 :height 400}
-
-    [:rect {:x 0
-            :y 0
-            :width 500
-            :height 500
-            :fill "black"}]
-
-    [:rect {:x 5
-            :y 5
-            :width 30
-            :height 30
-            :fill "#3498db"}]
-
-    [:rect {:x 37
-            :y 5
-            :width 30
-            :height 30
-            :fill "#3498db"}]]
-
-   ])
+  (let [squares (sort (map (juxt first second) @id))
+        sqs (loop [squares squares
+                   offset-x 0
+                   offset-y 0
+                   output [:g]]
+              (if (empty? squares)
+                output
+                (let [[index state] (first squares)
+                      new-row? (zero? (mod index grid-size))
+                      offset-x (if new-row? 0 offset-x)
+                      offset-y (if new-row? (+ offset-y 32) offset-y)]
+                  (recur (rest squares)
+                         (+ offset-x 32)
+                         offset-y
+                         (conj output [:rect {:x offset-x
+                                              :y offset-y
+                                              :width 30
+                                              :height 30
+                                              :fill (if (= :closed state)
+                                                      "black"
+                                                      "cyan")}])))))]
+    [:div
+     [:button {:on-click start!} "Start!"]
+     [:svg {:width 400 :height 400}
+      sqs]]))
 
 (reagent/render-component [view]
                           (js/document.getElementById "app"))
